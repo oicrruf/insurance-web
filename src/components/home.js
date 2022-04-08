e4eaba2842690589d2ad3c4c71215ae251d13c2a
Rogelio Galindo <roge.galindo@gmail.com>
2022-04-07 22:29:15 -0500

    home component created

---
+++ src/components/home.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+export class Home extends Component {
+
+
+    render (){
+        return (
+            <div className='mt5 d-flex justify-content-left'>
+                <h2>
+                    Hellow from home
+                </h2>
+            </div>
+        )
+
+    }
+}

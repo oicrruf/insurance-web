495a33637d866cc8f4aea119866030a4418937d0
Rogelio Galindo <roge.galindo@gmail.com>
2022-04-22 08:56:36 -0500

    New design

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+// import React, {Component} from 'react';
+// import '/App.css'
 // import logo from './logo.svg';
-import './App.css';
+import { Home } from './components/Home'
 
-import {Home} from './components/home'
+
 
 function App() {
   return (
-    <div className="container">
-     <Home />
-    </div>
+
+     <Home/>
+
   );
 }
 
---
+++ src/temaConfig.js
@@ -0,0 +1,18 @@
+ import { createMuiTheme } from '@mui/material/styles'
+
+
+
+ const theme = createMuiTheme ({
+    palette: {
+        primary:{
+            main:"#0054B4"
+        }
+        ,secondary:{
+            main:"#0D1C48"
+        }
+       
+    }
+
+ })
+
+ export default theme;

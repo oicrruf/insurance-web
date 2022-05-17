dfc9b2db86d403cabf977eb1473f7826e4a70c86
Víctor M. Reyes <vm.reyesal@gmail.comm>
2022-05-17 09:39:26 -0600

    feat: install and config react-router-dom@6

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App/App'
 import './components/App/styles.sass'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}/>
+      <Route path="/login" element={<h1>Login</h1>}/>
+      <Route path="/register" element={<h1>Register</h1>}/>
+    </Routes>
+  </BrowserRouter>
 )
